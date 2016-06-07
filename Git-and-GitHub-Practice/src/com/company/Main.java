0ca9130ddc77b82af7d0902fccedacc0fed04d69
Kitty Tang <Kittys.Tang@gmail.com>
2016-06-07 12:49:46 -0700

    added partner1's code to IntelliJ practice project

---
+++ Git-and-GitHub-Practice/src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        int one = 1;
+        int two = 2;
+        int three = 3;
+    }
+}
